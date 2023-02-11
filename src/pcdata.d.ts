d7481fc4b39ec2e334aa048d3a39d53a0d6fc116
nicejs-is-cool <xbgxtmp+nicejsverif@gmail.com>
2023-02-10 21:27:56 -0300

    aaaa i forgot git add

---
+++ src/pcdata.d.ts
@@ -0,0 +1,17 @@
+declare namespace PCd {
+	export namespace Ultimatelogon {
+		export interface DeviceStartSession {
+			code: string;
+			token: string;
+		}
+		export interface DeviceDetails {
+			code: string;
+			user: {
+				username: string;
+				password: string;
+				token: string;
+				appdata: any;
+			}
+		}
+	}
+}
---
+++ src/ultimatelogon.ts
@@ -0,0 +1,25 @@
+export class DeviceAuth {
+	constructor(public code: string, private token: string) {}
+	async check() {
+		const resp = await fetch('https://ultimatelogon.pcprojects.tk/deviceDetails?device=' + this.token);
+		const json: PCd.Ultimatelogon.DeviceDetails = await resp.json();
+		return json;
+	}
+	wait() {
+		return new Promise((resolve, reject) => {
+			setInterval(() => {
+				this.check()
+					.then(data => {
+						if (data.user) return resolve(data);
+					})
+					.catch(err => reject(err));
+			}, 5000)
+		})
+	}
+}
+
+export async function StartDeviceAuth() {
+	const deviceSession: PCd.Ultimatelogon.DeviceStartSession =
+		await (await fetch("https://ultimatelogon.pcprojects.tk/deviceStartSess")).json();
+	return new DeviceAuth(deviceSession.code, deviceSession.token);
+}

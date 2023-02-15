cf42675edbf24d52afb4672d6ba5e974c94e049b
nicejs-is-cool <xbgxtmp+nicejsverif@gmail.com>
2023-02-15 13:12:04 -0300

    more api functions to wrapper

--- src/module.ts
+++ src/module.ts
@@ -7,6 +7,17 @@ export interface ServerContainer {
 	vmname_encoded: string;
 	status: Status;
 }
+export enum Distro {
+	debian,
+	archlinux,
+	"duckcloud/suspiral"
+}
+export class HTTPError extends Error {
+	constructor(message: string, public statuscode: number) {
+		super(message);
+		this.name = "HTTPError";
+	}
+}
 export class User {
 	constructor(public token: string, public duckcloud: DuckCloud) {}
 	async GetContainers(): Promise<Container[]> {
@@ -22,10 +33,52 @@ export class User {
 		
 		return pcontainers;
 	}
+	/**
+	 * Creates a container.
+	 * @param name Name of the container
+	 * @param network Network enabled
+	 * @param pro Pro mode
+	 * @param distro Distribution of linux to be installed on the container
+	 * @returns HTTP Status Code
+	 */
+	async CreateContainer(name: string, network: boolean, pro: boolean, distro: Distro): Promise<number> {
+		const body = new URLSearchParams();
+		body.append('vm_name', name);
+		body.append('shouldHaveNetworking', network.toString());
+		body.append('shouldUse512mbRAM', pro.toString());
+		body.append('distro', distro.toString())
+		const resp = await fetch(this.duckcloud.server+'/newVM', {
+			method: 'POST',
+			headers: { Cookie: `token=${this.token}` },
+			body
+		});
+		return resp.status
+	}
+	
 }
 export class Container {
 	constructor(public id: number, public name: string, public status: Status, public duckcloud: DuckCloud) {}
-	
+	/**
+	 * Toggle a Container's power.
+	 * @returns Boolean indicating success when true
+	 */
+	TogglePower(): Promise<boolean> {
+		return fetch(this.duckcloud.server+"/shutoff/"+this.id,
+			{ headers: { Cookie: `token=${this.duckcloud.user.token}`}})
+				.then(resp => resp.text())
+				.then(data => {
+					if (data.includes('Sorry, your VM failed to launch')) {
+						// VeNT
+						return false;
+					}
+					return true;
+				})
+	}
+	Remove(): Promise<boolean> {
+		return fetch(`${this.duckcloud.server}/burn/${this.id}`, {
+			headers: { Cookie: `token=${this.duckcloud.user.token}` }
+		}).then(resp => resp.status === 200);
+	}
 }
 export class DuckCloud {
 	public user!: User;
